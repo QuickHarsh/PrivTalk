a938b302e515b9f936dc080baf1c5a0316d7390e
QuickHarsh <hp1659414@gmail.com>
2025-05-03 02:28:06 +0530

    fifth commit

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -4,8 +4,13 @@ import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/mes
 
 const router = express.Router();
 
+// Get all users for sidebar
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:userId", protectRoute, getMessages);
-router.post("/send/:userId", protectRoute, sendMessage);
+
+// Get messages for a specific user
+router.get("/:userId([a-f0-9]{24})", protectRoute, getMessages);
+
+// Send message to a specific user
+router.post("/send/:userId([a-f0-9]{24})", protectRoute, sendMessage);
 
 export default router;
