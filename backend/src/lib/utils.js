ce77d40fb7c374270f69d2b726a0225a39988c43
Harsh Patel <hp1659414@gmail.com>
2026-02-19 17:48:47 +0530

    added vercel.json

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d"
     })
-    res.cookie("jwt",token, {
+    res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //17 in ms
         httpOnly: true, //prevent xss attack cross-site scripting attacks
-        sameSite: "strict", // CSRF attack cross-site request forgery attacks
+        sameSite: process.env.NODE_ENV === "development" ? "strict" : "none",
         secure: process.env.NODE_ENV !== "development",
     })
 
